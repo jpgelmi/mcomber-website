4b44bc7dc62ef09e0cf106b1169b455c1c86c908
Juan <you@example.com>
2021-01-31 01:48:12 -0300

    commit

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -3,32 +3,21 @@ import { Row, Col } from "react-flexbox-grid";
 import "./about.scss";
 // Components
 import HeroImage from "./hero-image.png";
-import Logo from "./logo_trans.png";
 import Title from "../ui-components/title/title";
 
 const about = () => (
   <div id="about">
     <div className="wrapper">
       <Title title="SOBRE MÍ." />
-      <Row className= "trans-container">
+      <Row>
         <Col md={12} lg={6}>
           <p className="font20">
-            Soy Michael Comber, tengo 28 años, vivo hace más de veinte años en
-            Lo Barnechea y crecí lleno de experiencias que me llenan de orgullo
-            a la vez que me dejan muy en claro los desafíos a los cuales nos
-            debemos dedicar. Soy Arquitecto y Magíster en Proyecto Urbano de la
-            PUC. Tuve la alegría de liderar la Organización Vecinal Plan 18
-            durante 5 años, con la cual nos dedicamos a generar proyectos de
-            desarrollo urbano y social en el Cerro 18, experiencia que luego me
-            llevó a titularme con un proyecto enfocado en el desarrollo integral
-            del cerro y sus alrededores.
-          </p>
-          <p className="font20 pad">
-            Trabajé en la SECPLA de Lo Barnechea y actualmente soy arquitecto en
-            el Ministerio de Vivienda y Urbanismo, lugar en el cual me he
-            desarrollado profesionalmente y desde donde veo la tremenda
-            influencia que tiene la ciudad como motor de oportunidades y una
-            mejor calidad de vida.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididuntut labore et dolore magna aliqua. Lorem
+            ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididuntut labore et dolore magna aliqua. Lorem ipsum dolor
+            sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididuntut labore et dolore magna aliqua.
           </p>
         </Col>
         <Col md={12} lg={6}>
@@ -36,9 +25,6 @@ const about = () => (
             <img src={HeroImage} alt="hero" />
           </div>
         </Col>
-        <div className="image-trans">
-          <img src={Logo} alt="hero" />
-        </div>
       </Row>
     </div>
   </div>
--- src/components/combersemos/comber.jsx
+++ src/components/combersemos/comber.jsx
@@ -4,33 +4,11 @@ import "./comber.scss";
 // Components
 import Title from "../ui-components/title/title";
 
+import Cubillos from "./fotos/cubillos.png"
+
 const comber = () => (
   <div id="comber">
-    <div className="wrapper">
-      <Title title="SOBRE MÍ." />
-      <Row className= "trans-container">
-        <Col md={12} lg={6}>
-          <p className="font20">
-            Soy Michael Comber, tengo 28 años, vivo hace más de veinte años en
-            Lo Barnechea y crecí lleno de experiencias que me llenan de orgullo
-            a la vez que me dejan muy en claro los desafíos a los cuales nos
-            debemos dedicar. Soy Arquitecto y Magíster en Proyecto Urbano de la
-            PUC. Tuve la alegría de liderar la Organización Vecinal Plan 18
-            durante 5 años, con la cual nos dedicamos a generar proyectos de
-            desarrollo urbano y social en el Cerro 18, experiencia que luego me
-            llevó a titularme con un proyecto enfocado en el desarrollo integral
-            del cerro y sus alrededores.
-          </p>
-          <p className="font20 pad">
-            Trabajé en la SECPLA de Lo Barnechea y actualmente soy arquitecto en
-            el Ministerio de Vivienda y Urbanismo, lugar en el cual me he
-            desarrollado profesionalmente y desde donde veo la tremenda
-            influencia que tiene la ciudad como motor de oportunidades y una
-            mejor calidad de vida.
-          </p>
-        </Col>
-      </Row>
-    </div>
+    
   </div>
 );
 
