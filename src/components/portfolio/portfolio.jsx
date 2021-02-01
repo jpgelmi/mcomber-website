b9716caed6237866dde63cee7da9353b42367116
Juan <you@example.com>
2021-01-31 23:51:12 -0300

    commit

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -4,125 +4,39 @@ import Masonry from "react-masonry-css";
 //Scss
 import "./portfolio.scss";
 //Assets
-import Preview1 from "../../assets/portfolio/project01/preview.png";
-import Preview2 from "../../assets/portfolio/project02/preview.png";
-import Preview3 from "../../assets/portfolio/project03/preview.png";
-import Preview4 from "../../assets/portfolio/project04/preview.png";
-import Preview5 from "../../assets/portfolio/project05/preview.png";
-import Preview6 from "../../assets/portfolio/project06/preview.png";
+import Preview1 from "../../assets/portfolio/foto7.jpeg";
+import Preview2 from "../../assets/portfolio/foto3.jpeg";
+import Preview3 from "../../assets/portfolio/foto4.jpeg";
+import Preview4 from "../../assets/portfolio/foto5.jpeg";
+import Preview5 from "../../assets/portfolio/foto6.jpeg";
+import Preview6 from "../../assets/portfolio/foto8.jpeg";
 //Components
 import Title from "../ui-components/title/title";
 import ProjectBox from "../ui-components/projectBox/projectBox";
 
-class Portfolio extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      // PORTFOLIO PROJECTS
-      projects: [
-        {
-          id: "1",
-          preview: Preview1,
-          title: "Lamp",
-          tag: "branding",
-        },
-        {
-          id: "2",
-          preview: Preview2,
-          title: "Smartwatch",
-          tag: "web",
-        },
-        {
-          id: "3",
-          preview: Preview3,
-          title: "Speakerphone",
-          tag: "illustrations",
-        },
-        {
-          id: "4",
-          preview: Preview4,
-          title: "Sneakers",
-          tag: "web",
-        },
-        {
-          id: "5",
-          preview: Preview5,
-          title: "Label",
-          tag: "illustrations",
-        },
-        {
-          id: "6",
-          preview: Preview6,
-          title: "lemons",
-          tag: "branding",
-        },
-      ],
-      // PORTFOLIO GALLERY WILL LOAD THIS AFTER FUNCTION "filterGallery" FINISH FILTERING
-      filterResult: null,
-      pickedFilter: "all",
-      filterMenuActive: false,
-      pickedFilterDropdown: "NEWEST"
-    };
-  }
-
-  // FIRST LOAD
-  componentDidMount() {
-    this.filterGallery("all");
-  }
-
-  //FILTER PORTFOLIO FUNCTION
-  filterGallery = (target) => {
-    let projectsArr = [...this.state.projects];
-    let result;
-
-    if (target !== "all") {
-      result = projectsArr.filter((project) => project.tag === target);
-    } else {
-      result = projectsArr;
-    }
-
-    this.setState({ filterResult: result, pickedFilter: target, pickedFilterDropdown: "NEWEST" });
-  };
-
-  // FILTER DROP DOWN HOVER MENU FUNCTION
-  filterMenuHover = (event) => {
-    if(event) {
-      this.setState({ filterMenuActive: true });
-    }else {
-      this.setState({ filterMenuActive: false });
-    }
-  }
-  // RENDER
-  render() {
-    // PORTFOLIO GALLERY RENDER
-    let projectsRender = null;
-    if (this.state.filterResult) {
-      projectsRender = this.state.filterResult.map((project) => (
-        <ProjectBox preview={project.preview} key={project.id} title={project.title} tag={project.tag} />
-      ));
-    }
-    // PORTFOLIO GALLERY BREAKPOINTS
-    const portfolioBreakpoints = {
-      default: 3,
-      1100: 3,
-      700: 2,
-      500: 1,
-    };
-
-    return (
-      <div id="portfolio">
-        <div className="wrapper">
-          <Title title="EN TERRENO." />
-          <Row>
-            <Col xs={12} sm={12} md={4} lg={3}/>
-          </Row>
-          <Masonry breakpointCols={portfolioBreakpoints} className="my-masonry-grid" columnClassName="mint__gallery">
-            {projectsRender}
-          </Masonry>
-        </div>
+const Portfolio = () => (
+  <div className = "position-cont">
+    <div class="gallery cf">
+      <div className= "img-cont">
+        <img src={Preview1} />
       </div>
-    );
-  }
-}
+      <div className= "img-cont">
+        <img src={Preview2} />
+      </div>
+      <div className= "img-cont">
+        <img src={Preview3} />
+      </div>
+      <div className= "img-cont">
+        <img src={Preview4} />
+      </div>
+      <div className= "img-cont">
+        <img src={Preview5}/>
+      </div>
+      <div className= "img-cont">
+        <img src={Preview6}/>
+      </div>
+    </div>
+  </div>
+);
 
 export default Portfolio;
