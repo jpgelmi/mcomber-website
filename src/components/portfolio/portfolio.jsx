240d89a8c91625ea680b4d4b503d5fb00309312a
Juan <you@example.com>
2021-02-01 00:20:31 -0300

    commit

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -4,36 +4,40 @@ import Masonry from "react-masonry-css";
 //Scss
 import "./portfolio.scss";
 //Assets
-import Preview1 from "../../assets/portfolio/foto7.jpeg";
 import Preview2 from "../../assets/portfolio/foto3.jpeg";
-import Preview3 from "../../assets/portfolio/foto4.jpeg";
 import Preview4 from "../../assets/portfolio/foto5.jpeg";
 import Preview5 from "../../assets/portfolio/foto6.jpeg";
-import Preview6 from "../../assets/portfolio/foto8.jpeg";
+import Preview6 from "../../assets/portfolio/video1.mp4";
 //Components
 import Title from "../ui-components/title/title";
-import ProjectBox from "../ui-components/projectBox/projectBox";
 
 const Portfolio = () => (
-  <div className = "position-cont">
+  <div className="position-cont" id="portfolio">
+    <Title title="EN TERRENO." />
     <div class="gallery cf">
-      <div className= "img-cont">
-        <img src={Preview1} />
-      </div>
-      <div className= "img-cont">
+      <div className="img-cont">
         <img src={Preview2} />
       </div>
-      <div className= "img-cont">
-        <img src={Preview3} />
-      </div>
-      <div className= "img-cont">
+      <div className="img-cont">
         <img src={Preview4} />
       </div>
-      <div className= "img-cont">
-        <img src={Preview5}/>
+      <div className="img-cont">
+        <img src={Preview5} />
       </div>
-      <div className= "img-cont">
-        <img src={Preview6}/>
+      <div className="vid-cont2">
+        <video className="vid">
+          <source src={Preview6} />
+        </video>
+      </div>
+      <div className="vid-cont2">
+        <video className="vid">
+          <source src={Preview6} />
+        </video>
+      </div>
+      <div className="vid-cont2">
+        <video className="vid">
+          <source src={Preview6} />
+        </video>
       </div>
     </div>
   </div>
