fee6e92bb56058a223498b7d1eb24299f682344d
Juan Pablo Gelmi <jpgelmi@gmail.com>
2021-02-08 23:01:28 -0300

    commit

--- src/components/blog/blog.jsx
+++ src/components/blog/blog.jsx
@@ -1,42 +1,34 @@
 import React from "react";
-import { Row, } from "react-flexbox-grid";
+import { Row, Col } from "react-flexbox-grid";
 import "./blog.scss";
 //Components
 import Title from "../ui-components/title/title";
+
+import Vivienda from "./videos/Vivienda.mp4";
 
 const Blog = () => (
   <div id="blog" className="about-2">
     <div className="wrapper">
       <Title title="PROPUESTAS" />
-      <Row>
-        <div className="font70 propuestas-cont">EL DESAFÍO DE LA VIVIENDA</div>
-        <p className="font20 parrafo">
-          Lograr dar acceso a vivienda accesible a nuestros vecinos allegados y
-          en campamentos es una prioridad social urgente. Frente al escenario de
-          la falta de suelo y el alto precio de éste se deben desarrollar
-          múltiples caminos con miras a lograr la meta.
-        </p>
-        <div className="font70 propuestas-cont despues">
-          DESARROLLO URBANO INTEGRAL
-        </div>
-        <p className="font20 parrafo">
-          La ciudad no puede seguir avanzando sobre los cerros, más bien
-          esperamos que la naturaleza de esos cerros entre en nuestra ciudad. La
-          protección de nuestro entorno y el desarrollo de las áreas verdes
-          asociadas a cerros y esteros es crucial para la comuna que queremos
-          ser en el futuro.
-        </p>
-        <div className="font70 propuestas-cont despues">
-          SOCIEDAD CIVIL ACTIVA
-        </div>
-        <p className="font20 parrafo">
-          El deporte y la cultura, cuando se practican cotidianamente con un
-          guía responsable, son las herramientas más potentes que tenemos para
-          prevenir otros malos hábitos, como son el consumo de drogas. Debemos
-          financiar y apoyar todas las iniciativas que sean un aporte social en
-          este sentido. Debemos financiar y apoyar todas las iniciativas de la
-          sociedad civil que sean un aporte a nuestra comunidad.
-        </p>
+      <Row className="trans-container">
+        <Col md={12} lg={6}>
+          <p className="font20">
+            Soy Michael Comber, tengo 28 años, soy Arquitecto y Magíster en
+            Proyecto Urbano de la PUC. Tuve la alegría de liderar la
+            Organización Vecinal Plan 18 durante 5 años, con la cual nos
+            dedicamos a generar proyectos de desarrollo urbano y social en el
+            Cerro 18, experiencia que luego llevó a a que me titulara con un
+            proyecto enfocado en el desarrollo integral del cerro y sus
+            alrededores.
+          </p>
+        </Col>
+        <Col md={12} lg={6}>
+          <div className="vid-cont2">
+            <video className="vid" controls>
+              <source src={Vivienda} />
+            </video>
+          </div>
+        </Col>
       </Row>
     </div>
   </div>
